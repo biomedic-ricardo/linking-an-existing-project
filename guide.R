b2d9451d7b7a09a06666e550f39c3e69ec79d0df
biomedic-ricardo <ricardo.baezma@outlook.es>
2019-09-22 18:57:46 -0500

    guide to join you to a new project in github

---
+++ guide.R
@@ -0,0 +1,5 @@
+# Working on an existing GitHub repository
+# If there is an existing project that others are working on that you are asked to contribute to, you can link the existing project with your RStudio. It follows the exact same premise as that from the last lesson where you created a GitHub repository and then cloned it to your local computer using RStudio. In brief, in RStudio, go to File > New Project > Version Control. Select Git as your version control system, and like in the last lesson, provide the URL to the repository that you are attempting to clone and select a location on your computer to store the files locally. Create the project.
+# Clone an existing project from GitHub from within RStudio
+# 
+# All the existing files in the repository should now be stored locally on your computer and you have the ability to push edits from your RStudio interface. The only difference from the last lesson is that you did not create the original repository, instead you cloned somebody else’s.
